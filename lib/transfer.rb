d82658dc57a74116475fff04e70dfb300d8ad101
Steve Anderson <prototype909@msn.com>
2020-05-20 03:53:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,13 +18,13 @@ class Transfer
 
   def execute_transaction
     def execute_transaction
-    if @sender.balance > @amount && @status == "pending" && valid?
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    else
-      @status = "rejected"
+      if @sender.balance > @amount && @status == "pending" && valid?
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        @status = "complete"
+      else
+        @status = "rejected"
       return "Transaction rejected. Please check your account balance."
+      end
     end
-  end
 end
