0d4367d43304138bad4ec43cc8962fe654037a97
Steve Anderson <prototype909@msn.com>
2020-05-20 03:50:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,9 +17,13 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance < @amount
+    def execute_transaction
+    if @sender.balance > @amount && @status == "pending" && valid?
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
-  end
 end
